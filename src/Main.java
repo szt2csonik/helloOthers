2abe9f9d5820651198ff5ef3e196f6171ddf06f5
szt2csonik <szt2csonik@vasvari.org>
2023-09-26 12:09:15 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Péter!");
+        System.out.println("Hello, Szabolcs!");
+        System.out.println("Hello, Márk!");
+    }
+}
